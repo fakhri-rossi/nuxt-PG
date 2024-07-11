9c2dc8b3ec904032338061007f1257970fa21ff0
Fakhri Rossi <rossifakhri@gmail.com>
2024-07-11 12:35:20 +0700

    Installed nuxt ui, it includes: nuxt ui, tailwindcss, color mode, and icon

---
+++ components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-full flex justify-center items-center">
+
+    <div class="flex justify-between items-center h-[50px] w-full md:w-[80%] px-2 border-b">
+      <div class="flex items-center gap-5">
+        <NuxtLink v-for="(menu, i) in menus" :to="menu.to">{{menu.label}}</NuxtLink>
+      </div>
+  
+      <div class="flex gap-3">
+        <UButton variant="ghost">Sign in</UButton>
+        <UButton variant="solid">Sign Up</UButton>
+        <ToggleColorMode />
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+  const menus = [
+    {label: 'Home', to: '/'},
+    {label: 'Videos', to: '/'},
+    {label: 'Dashboard', to: '/'},
+  ]
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex">
+    <p class="text-3xl">halo</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  definePageMeta({
+    layout: 'page'
+  })
+</script>
+
+<style>
+
+</style>
